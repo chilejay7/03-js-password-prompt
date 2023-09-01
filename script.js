72d78f1fb86f5869fbcc24489590f17653b2ed8f
Cody Burkholder <codyburk7@yahoo.com>
2023-09-01 14:06:23 -0600

    Incorporates character prompt into function

--- script.js
+++ script.js
@@ -15,21 +15,23 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //Issue #1 - Open prompt on click
+// The function below was added to the attributes of the button element in the html.
 function passPrompt() {
-  var click = prompt("Thank you for having a good security first mindset.  Please select what you would like to include in your pasword from the following prompts.")
+  var click = alert("Thank you for creating a strong password.  Please select what you would like to include in your password from the prompts that follow.  Please press Ok to continue.")
+
+  // Issue #3 - Add a prompt for the number of characters user is requesting.
+  // The parse integer function is used with the prompt to give the value entered a type of number as it will deafult to a string.
+  // The while function will continue to prompt until a valid value is entered.  If text is entered it will be caught
+  // by the !numCtrs expression since it would return NaN when trying to parse which is falsy.
+  // An or expression was added to also catch numbers less than 8 or numbers greater than 128.
+
+  let numCtrs = parseInt(prompt('How many characters would you like to include in your password?  Please enter a number between 8 and 128.'));
+
+  while (!numCtrs || numCtrs < 8 || numCtrs > 128) {
+  numCtrs = parseInt(prompt(`You entered: ${numCtrs}. Please enter a valid number between 8 - 128.`));
+  }
+
 }
-
-// Issue #3 - Add a prompt for the number of characters user is requesting.
-// The parse integer function is used with the prompt to give the value entered a type of number as it will deafult to a string.
-// The while function will continue to prompt until a valid value is entered.  If text is entered it will be caught
-// by the !numCtrs expression since it would return NaN when trying to parse which is falsy.
-// An or expression was added to also catch numbers less than 8 or numbers greater than 128.
-
-// let numCtrs = parseInt(prompt('How many characters would you like to include in your password?  Please enter a number between 8 and 128.'));
-
-// while (!numCtrs || numCtrs < 8 || numCtrs > 128) {
-//   numCtrs = parseInt(prompt("Please enter a valid number between 8 - 128."));
-// }
 
 
 
